a30a0fcf6bd3aafead90b9c1dcceccdbc9ec2a48
3ter <drinc3@gmail.com>
2020-05-26 22:21:53 +0200

    add symlink recreator

---
+++ update_symlinks.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use v5.30;
+
+sub update_symlink {
+    my ($arg_ref) = @_;
+
+    my $target  = $arg_ref -> {'target'} or die 'named argument "target" missing';
+    my $link    = $arg_ref -> {'link'} or die 'named argument "link" missing';
+
+    if (($target . $link) =~ /\~/) {
+        say "'~' can't be expanded by perl's file tests. Check the constant SYMLINKS.";
+        return 1;
+    }
+
+    if (-l $link && -e $link) {
+        say 'Link exists and is valid';
+        return 0;
+    }
+    elsif (-l $link && !(-e $link)) {
+        say 'Link exists but is invalid, removing bad link: ' . $link;
+        say qx(/usr/bin/rm $link);
+    }
+    
+    if (!(-e $target)) {
+        say "Can't create link for non existing target: $target";
+        return 1;
+    }
+
+    say qx(/usr/bin/ln -s $target $link);
+    return;
+}
+
+use constant SYMLINKS => {
+    '/home/dennis/ecos/ecos_start_vpn.pl' => '/home/dennis/.local/bin/ecos_mos',
+};
+
+foreach my $target (keys %{ SYMLINKS() }) {
+    my $link = SYMLINKS() -> {$target}; 
+    say "Update symlink $link pointing to $target";
+    update_symlink({
+        'target' => $target,
+        'link' => $link,
+    });
+}
+
