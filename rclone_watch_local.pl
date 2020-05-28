85b46f713b97d601572a4974f564c820dae78535
3ter <drinc3@gmail.com>
2020-05-28 13:06:04 +0200

    include option to first sync and error notification on desktop

--- rclone_watch_local.pl
+++ rclone_watch_local.pl
@@ -5,6 +5,10 @@ use warnings;
 use v5.30;
 
 use English;
+use Getopt::Long;
+
+my $is_first_sync = 0;
+GetOptions ('first-sync' => \$is_first_sync) or die("Error in command line arguments\n");;
 
 our $local_dir      = '/home/dennis/googledrive/Sicherheit';
 our $remote_dir     = 'googledrive:/Sicherheit';
@@ -13,6 +17,32 @@ our $inotify_opts   =
     "--event " .
     join(' --event ', qw(modify attrib close_write moved_to create delete)) .
     " --recursive $local_dir";
+
+sub run_sync {
+    my ($is_first_sync) = @_;
+
+    if ($is_first_sync) {
+        say "You've specified '--first-sync'. This will overwrite files in $remote_dir ".
+        "with file from $local_dir. Is this what you want? (y/n): ";
+        
+        my $answer = '';
+        while (1) {
+            $answer = <STDIN>;
+            chomp $answer;
+            last if ($answer =~ /^(?:y|n)$/);
+            say "Please only type 'y' or 'n' and press <Enter>.";
+        }
+
+        if ($answer eq 'y') {
+            say qx( python3 $rclonesync_exe --first-sync --verbose $local_dir $remote_dir 2>&1 );        
+        }
+    }
+
+    my $rclonesync_rc = (system("python3 $rclonesync_exe --verbose $local_dir $remote_dir 2>&1") >> 8);
+    system("/usr/bin/notify-send --urgency critical 'Error in rclone-sync' 'Ended with return code $rclonesync_rc'") if ($rclonesync_rc);
+
+    return;
+}
 
 sub wait_until_file_silent
     {
@@ -30,7 +60,7 @@ sub watch_folder_and_sync_on_sched
     if ( (system( "inotifywait --timeout 300 $inotify_opts" ) >> 8) != 1 )
         {
         wait_until_file_silent();
-        say qx( python3 $rclonesync_exe --verbose $local_dir $remote_dir 2>&1 );
+        run_sync(); 
         }
     else
         {
@@ -39,12 +69,17 @@ sub watch_folder_and_sync_on_sched
     return;
     }
 
+if ($is_first_sync) {
+    run_sync($is_first_sync);
+    exit;
+}
+
 say '[rclone] Watching locally.';
 
 if (-d $local_dir)
     {
     say 'Sync on startup';
-    say qx( python3 $rclonesync_exe --verbose $local_dir $remote_dir 2>&1 );
+    run_sync();
     while (1)
         {
         watch_folder_and_sync_on_sched();
